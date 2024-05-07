c51d58cdb29f4a32353b5b34396bf8777988b1ab
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-05-07 17:08:35 -0300

    implementando modo escuro

--- modoescuro.js
+++ modoescuro.js
@@ -1,6 +1,22 @@
 const temaescuro = document.getElementById('tema-escuro');
 const body = document.body;
-
 temaescuro.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
+function aumentartexto() {
+    const elemento = document.getElementById("texto");
+    const tamanhoAtual = parseInt(window.getComputedStyle(elemento, null).getPropertyValue('font-size'));
+    const novoTamanho = tamanhoAtual + 1;
+    elemento.style.fontSize = novoTamanho + 'px';
+}
+
+
+const botao = document.getElementsByTagName('button');
+
+
+botao[0].addEventListener('click', () => document.body.style.fontSize = '10px');
+
+botao[1].addEventListener('click', () => document.body.style.fontSize = '15px');
+
+
+botao[2].addEventListener('click', () => document.body.style.fontSize = '20px');
